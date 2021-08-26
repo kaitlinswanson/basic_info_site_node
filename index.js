90edfc46b776e6bdbc8ddcf3c80afc0c29fc65d4
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-26 15:07:12 -0700

    error page working

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
         );
       }
     
-    if (req.url ==='/about') { 
+    else if (req.url ==='/about') { 
         fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => { 
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -21,14 +21,25 @@ const server = http.createServer((req, res) => {
       );
     }
 
-    if(req.url === '/contact-me') {
+    else if(req.url === '/contact-me') {
         fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => { 
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(content);  
            }
         );
-    }
+    
+
+ } else { 
+     if(req.url !== '/contact-me' || '/about' || '/') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => { 
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        }
+    );
+  }
+ }
 });
     
 
