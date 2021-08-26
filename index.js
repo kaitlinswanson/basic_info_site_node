a39bf3fb8f16a3153622c90e26d8aaec79678398
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-26 14:50:48 -0700

    all content pages working

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => { 
     if (req.url === '/') { 
-         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => { 
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => { 
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-            
+            res.end(content);   
+          }
+        );
+      }
+    
+    if (req.url ==='/about') { 
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => { 
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);  
         }
-    )}
-})
+      );
+    }
 
-const PORT = process.env.PORT || 5000; 
+    if(req.url === '/contact-me') {
+        fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, content) => { 
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);  
+           }
+        );
+    }
+});
+    
+
+const PORT = process.env.PORT || 8080; 
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
